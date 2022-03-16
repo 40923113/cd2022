487fa75615e6573c1776e788eb2dc782d2f447ad
40923113 <40923113@gm.nfu.edu.tw>
2022-03-16 09:38:40 +0800

    W1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test \n', 'tags': '', 'url': 'About.html'}, {'title': 'NX1980 H1', 'text': '', 'tags': '', 'url': 'NX1980 H1.html'}, {'title': 'note', 'text': '', 'tags': '', 'url': 'note.html'}, {'title': '建立倉儲', 'text': '先尋找老師倉儲的cmsimde site \xa0 \n \n 接著複製老師的網址 \n', 'tags': '', 'url': '建立倉儲.html'}]};
